6bc55488ae7563741d853664391f650aac48ea60
HJOC15 <humbertorellana15@gmail.com>
2021-09-10 23:54:19 -0600

    finalizadooo

--- src/main.java
+++ src/main.java
@@ -4,14 +4,7 @@ public class main {
 
     public static void main(String args[]){
 
-        Scanner key = new Scanner(System.in);
-        System.out.println("Ingresa el primer numero");
-        int primero = key.nextInt();
-
-        System.out.println("Ingresa el segundo numero");
-        int segundo = key.nextInt();
-
-        System.out.println("La suma de " + primero + " y " + segundo + " es: " + (primero+segundo));
+        System.out.println("Fin");
     }
 
 }
