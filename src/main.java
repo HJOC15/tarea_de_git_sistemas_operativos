94722839a5450f028d7bec1eb6846c691cecc362
HJOC15 <humbertorellana15@gmail.com>
2021-09-10 23:46:29 -0600

    mensaje simple

--- src/main.java
+++ src/main.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String args[]){
-        Scanner key = new Scanner(System.in);
-        System.out.println("Imprimir tablas que elijas");
-        System.out.print("Ingresa opcion: ");
-        int op = key.nextInt();
 
-        for (int i = 1; i < 13; i++) {
-            System.out.println(i + " x 9 =" + i*op);
-        }
+        System.out.println("Mensaje simple");
 
     }
 
