8eee1ae176b76186aafb441881a71e3f62dcc379
HJOC15 <humbertorellana15@gmail.com>
2021-09-10 23:54:02 -0600

    Ultimo commit

--- src/main.java
+++ src/main.java
@@ -4,8 +4,14 @@ public class main {
 
     public static void main(String args[]){
 
-        System.out.println("Cuarta version");
+        Scanner key = new Scanner(System.in);
+        System.out.println("Ingresa el primer numero");
+        int primero = key.nextInt();
 
+        System.out.println("Ingresa el segundo numero");
+        int segundo = key.nextInt();
+
+        System.out.println("La suma de " + primero + " y " + segundo + " es: " + (primero+segundo));
     }
 
 }
